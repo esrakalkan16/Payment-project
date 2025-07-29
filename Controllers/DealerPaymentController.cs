f15b383d6142b5896a32698a95b405f824101e34
Esra <esra.kalkan1621@gmail.com>
2025-07-29 13:39:47 +0300

    Çıkan sorunlar halledildi.

--- Controllers/DealerPaymentController.cs
+++ Controllers/DealerPaymentController.cs
@@ -3,35 +3,12 @@ using Payment_project.PaymentCore.Helpers;
 using Payment_project.PaymentCore.Models;
 using PaymentCore.Services;
 using System.Threading.Tasks;
-using System;
 
 namespace Payment_project.Controllers
 {
     public class DealerPaymentController : Controller
     {
-        private readonly BakiyemPaymentService _paymentService;
-        private const string DealerCode = "250";
-        private const string Username = "testuser";
-        private const string Password = "KLRSMGLSX";
-
-        public DealerPaymentController()
-        {
-            _paymentService = new BakiyemPaymentService();
-            InitializeAuthentication();
-        }
-
-        private void InitializeAuthentication()
-        {
-            var checkKey = HashHelper.SHA256Hash(DealerCode + "MK" + Username + "PD" + Password);
-            var authentication = new PaymentDealerAuthentication
-            {
-                DealerCode = DealerCode,
-                Username = Username,
-                Password = Password,
-                CheckKey = checkKey
-            };
-            _paymentService.SetAuthentication(authentication);
-        }
+        private readonly BakiyemPaymentService _paymentService = new BakiyemPaymentService();
 
         [HttpGet]
         public ActionResult Pay3d()
@@ -42,76 +19,42 @@ namespace Payment_project.Controllers
         [HttpPost]
         public async Task<ActionResult> Pay3d(PaymentDealerRequest model)
         {
+            var dealerCode = "250";
+            var username = "testuser";
+            var password = "KLRSMGLSX";
+
+            var checkKey = HashHelper.SHA256Hash(dealerCode + "MK" + username + "PD" + password);
+
+            var request = new DealerPaymentServicePaymentRequest
+            {
+                PaymentDealerAuthentication = new PaymentDealerAuthentication
+                {
+                    DealerCode = dealerCode,
+                    Username = username,
+                    Password = password,
+                    CheckKey = checkKey
+                },
+                PaymentDealerRequest = model
+            };
+
+            DealerPaymentServicePaymentResult result;
             try
             {
-                if (!ModelState.IsValid)
-                {
-                    return View(model);
-                }
-
-                model.ClientIP = Request.UserHostAddress;
-                model.RedirectUrl = Url.Action("PayResult", "DealerPayment", null, Request.Url.Scheme);
-                
-                var result = await _paymentService.Start3DPaymentAsync(model);
-
-                if (result.ResultCode == "Success")
-                {
-                    return Redirect(result.Data); // 3D ödeme sayfasına yönlendirme
-                }
-
-                ViewBag.ErrorMessage = result.ResultMessage;
-                return View(model);
+                result = await _paymentService.Start3DPaymentAsync(request);
             }
-            catch (Exception ex)
+            catch
             {
-                ViewBag.ErrorMessage = "Ödeme işlemi sırasında bir hata oluştu: " + ex.Message;
-                return View(model);
+                ViewBag.ErrorMessage = "Sunucuya bağlanırken bir hata oluştu.";
+                return View();
             }
-        }
 
-        [HttpPost]
-        public async Task<ActionResult> DirectPayment(PaymentDealerRequest model)
-        {
-            try
+            if (result.ResultCode == "Success")
             {
-                if (!ModelState.IsValid)
-                {
-                    return Json(new { success = false, message = "Geçersiz ödeme bilgileri" });
-                }
+                return Redirect(result.Data); // 3D ödeme sayfasına yönlendirme
+            }
 
-                model.ClientIP = Request.UserHostAddress;
-                
-                var result = await _paymentService.DirectPaymentAsync(model);
-
-                return Json(new { 
-                    success = result.ResultCode == "Success",
-                    message = result.ResultMessage,
-                    data = result.Data
-                });
-            }
-            catch (Exception ex)
-            {
-                return Json(new { success = false, message = "Ödeme işlemi sırasında bir hata oluştu: " + ex.Message });
-            }
-        }
-
-        [HttpGet]
-        public async Task<ActionResult> CheckPaymentStatus(string trxCode)
-        {
-            try
-            {
-                var result = await _paymentService.GetPaymentStatusAsync(trxCode);
-                return Json(new { 
-                    success = result.ResultCode == "Success",
-                    message = result.ResultMessage,
-                    data = result.Data
-                }, JsonRequestBehavior.AllowGet);
-            }
-            catch (Exception ex)
-            {
-                return Json(new { success = false, message = "Ödeme durumu kontrol edilirken bir hata oluştu: " + ex.Message }, 
-                    JsonRequestBehavior.AllowGet);
-            }
+            ViewBag.ErrorMessage = result.ResultMessage;
+            return View();
         }
 
         [HttpGet]
--- PaymentCore/Services/BakiyemPaymentService.cs
+++ PaymentCore/Services/BakiyemPaymentService.cs
@@ -1,100 +1,21 @@
-﻿using Flurl;
-using Flurl.Http;
+﻿using Flurl.Http;
 using Payment_project.PaymentCore.Models;
-using System;
 using System.Threading.Tasks;
 
 namespace PaymentCore.Services
 {
     public class BakiyemPaymentService
     {
-        private readonly string _baseUrl;
-        private readonly PaymentDealerAuthentication _authentication;
-        private readonly IFlurlClient _flurlClient;
+        private const string BaseUrl = "https://service.testmoka.com";
 
-        public BakiyemPaymentService(string baseUrl = "https://service.testmoka.com")
+        public async Task<DealerPaymentServicePaymentResult> Start3DPaymentAsync(DealerPaymentServicePaymentRequest request)
         {
-            _baseUrl = baseUrl;
-            _flurlClient = new FlurlClient(_baseUrl)
-                .WithTimeout(TimeSpan.FromSeconds(30))
-                .WithHeader("Accept", "application/json");
-        }
+            string url = $"{BaseUrl}/PaymentDealer/DoDirectPaymentThreeD";
+            var response = await url
+                .PostJsonAsync(request)
+                .ReceiveJson<DealerPaymentServicePaymentResult>();
 
-        public void SetAuthentication(PaymentDealerAuthentication authentication)
-        {
-            _authentication = authentication;
-        }
-
-        public async Task<DealerPaymentServicePaymentResult> Start3DPaymentAsync(PaymentDealerRequest paymentRequest)
-        {
-            var request = new DealerPaymentServicePaymentRequest
-            {
-                PaymentDealerAuthentication = _authentication,
-                PaymentDealerRequest = paymentRequest
-            };
-
-            try
-            {
-                return await _flurlClient
-                    .Request("/PaymentDealer/DoDirectPaymentThreeD")
-                    .PostJsonAsync(request)
-                    .ReceiveJson<DealerPaymentServicePaymentResult>();
-            }
-            catch (FlurlHttpException ex)
-            {
-                var error = await ex.GetResponseJsonAsync<DealerPaymentServicePaymentResult>();
-                if (error != null)
-                {
-                    return error;
-                }
-                throw;
-            }
-        }
-
-        public async Task<DealerPaymentServiceDirectPaymentResult> DirectPaymentAsync(PaymentDealerRequest paymentRequest)
-        {
-            var request = new DealerPaymentServicePaymentRequest
-            {
-                PaymentDealerAuthentication = _authentication,
-                PaymentDealerRequest = paymentRequest
-            };
-
-            try
-            {
-                return await _flurlClient
-                    .Request("/PaymentDealer/DoDirectPayment")
-                    .PostJsonAsync(request)
-                    .ReceiveJson<DealerPaymentServiceDirectPaymentResult>();
-            }
-            catch (FlurlHttpException ex)
-            {
-                var error = await ex.GetResponseJsonAsync<DealerPaymentServiceDirectPaymentResult>();
-                if (error != null)
-                {
-                    return error;
-                }
-                throw;
-            }
-        }
-
-        public async Task<DealerPaymentServicePaymentResult> GetPaymentStatusAsync(string trxCode)
-        {
-            try
-            {
-                return await _flurlClient
-                    .Request("/PaymentDealer/GetPaymentStatus")
-                    .SetQueryParam("trxCode", trxCode)
-                    .GetJsonAsync<DealerPaymentServicePaymentResult>();
-            }
-            catch (FlurlHttpException ex)
-            {
-                var error = await ex.GetResponseJsonAsync<DealerPaymentServicePaymentResult>();
-                if (error != null)
-                {
-                    return error;
-                }
-                throw;
-            }
+            return response;
         }
     }
 }
